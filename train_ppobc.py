4cc153fa4282e2c348b408efcdb057adbc26efb4
VediYD <vedi.yash@gmail.com>
2024-12-18 13:23:44 +1100

    update train_ppobc.py

--- train_ppobc.py
+++ train_ppobc.py
@@ -236,6 +236,7 @@ def get_observations(_obs_dict):
 def get_agent_obs(_both, _other):
     obs_ai, obs_hm = [], []
     for _i, _j in zip(_both, _other):
+        print(_i)
         obs_ai.append(caster(_i['both_agent_obs'][1 - _j]))
         obs_hm.append(caster(_i['both_agent_obs'][_j]))
 
